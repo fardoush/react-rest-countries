c72e38324e3fed5772a37d36c7df94133446e103
fardoush <fardoushjahanara@gmail.com>
2023-03-22 10:32:35 +0600

    project structure and available countries count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { useDebugValue, useEffect, useState } from 'react';
+
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <LoadCountries> </LoadCountries>
     </div>
   );
 }
 
+
+function LoadCountries(){
+  const [countries, setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    // .then(data => console.log(data))
+    .then(data => setCountries(data))
+  }, [])
+  return(
+    <div className="">
+      <h1 className="">Visiting Every Country of the World...!</h1>
+
+      <h3 className="">Available Countries: {countries.length}</h3>
+    </div>
+  )
+}
 export default App;
